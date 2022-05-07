32a0612cff774033eeb77a43c87fa2aa79e5ce12
kjivani <kjivani@cisco.com>
2022-05-07 21:09:11 +0530

    dark mode added (complete)

--- static/app.js
+++ static/app.js
@@ -78,3 +78,31 @@ const clockText = () => {
 };
 
 setInterval(clockText, 1000);
+
+const themeButton = document.getElementById("theme-button");
+const darkTheme = "dark-theme";
+const iconTheme = "bxs-sun";
+
+const selectedTheme = localStorage.getItem("selected-theme");
+const selectedIcon = localStorage.getItem("selected-icon");
+
+const getCurrentTheme = () =>
+    document.body.classList.contains(darkTheme) ? "dark" : "light";
+const getCurrentIcon = () =>
+    themeButton.classList.contains(iconTheme) ? "bxs-moon" : "bxs-sun";
+
+if (selectedTheme) {
+    document.body.classList[selectedTheme === "dark" ? "add" : "remove"](
+        darkTheme
+    );
+    themeButton.classList[selectedIcon === "bxs-moon" ? "add" : "remove"](
+        iconTheme
+    );
+}
+
+themeButton.addEventListener("click", () => {
+    document.body.classList.toggle(darkTheme);
+    themeButton.classList.toggle(iconTheme);
+    localStorage.setItem("selected-theme", getCurrentTheme());
+    localStorage.setItem("selected-icon", getCurrentIcon());
+});
